974eaefb215c6440460a4badd97511e33eadd2e3
Nameoftheyear786 <106028622+Nameoftheyear786@users.noreply.github.com>
2023-04-15 13:20:28 -0400

    Add files via upload

---
+++ garden.js
@@ -0,0 +1,27 @@
+status = "";
+
+function preload()
+{
+
+}
+
+function setup(){
+    canvas = createCanvas(640, 420);
+    canvas.center();
+    objectDetector = ml5.objectDetector('cocossd',modelLoaded);
+    document.getElementById('status').innerHTML = "Status: Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Loaded!")
+    status = true;
+    objectDetector.detect(img, gotResult);
+}
+
+function gotResults(error, results) {
+    if(error){
+        console.log(error);
+    }
+    console.log(results);
+    objects = results;
+}
